9e43fdd6ab60b1e9ad71e455ac283dd3c9b95abf
Dr. Kat <vertigodragon@gmail.com>
2025-04-16 23:42:20 -0400

    fixed loading centering

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,24 +2,23 @@ import { Box } from "@mantine/core";
 import Image from "next/image";
 
 export default function Loading() {
-  const height = 128;
-  const width = 128;
+  const size = 64;
 
   return (
     <Box
       className="absolute"
       style={{
-        top: `calc(50% - ${height / 2}px)`,
-        left: `calc(50% + ${width / 2}px)`,
+        top: `calc(50% - ${size / 2}px)`,
+        left: `calc(50% - ${size / 2}px)`,
       }}
     >
       <Image
         src="/loading.png"
         alt="loading"
-        className="translate-x-1/4 translate-y-1/4"
+        className="translate-y-1/2"
         style={{ animation: "spin 1.5s linear infinite" }}
-        width={width}
-        height={height}
+        width={size}
+        height={size}
       />
     </Box>
   );

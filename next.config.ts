7b72f5fcc5baff6aa8e00cf133883c68c67ce50b
Dr. Kat <vertigodragon@gmail.com>
2025-04-18 08:42:59 -0400

    added image cache ttl to reduce vercel image optimization transformation limit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    minimumCacheTTL: 2678400, // 31 days
+  },
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
